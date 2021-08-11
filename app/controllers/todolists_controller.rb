e4b60e576d3a0979029bf2bb2a4490ec03beed8e
EC2 Default User <ec2-user@ip-172-31-50-107.ec2.internal>
2021-08-11 08:02:07 +0000

    [Start]2-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -18,6 +18,8 @@ class TodolistsController < ApplicationController
     @list = List.find(params[:id])
   end
   
+  
+  
   private
   def list_params
     params.require(:list).permit(:title,:body)
